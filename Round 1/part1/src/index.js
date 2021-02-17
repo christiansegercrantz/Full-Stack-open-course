832d2916154e66c0a74cac1d4523a18a74d97329
Christian Segercrantz <segercc1@aalto.fi>
2021-02-17 20:34:29 +0200

    Started round 1

---
+++ Round 1/part1/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+
+  return (
+    <div>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
