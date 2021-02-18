faf1c013a952e6d94737edba213f7f0d7a819d68
Christian Segercrantz <segercc1@aalto.fi>
2021-02-18 17:25:31 +0200

    Completed Part 1 a

--- Round 1/part1/src/index.js
+++ Round 1/part1/src/index.js
@@ -1,17 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Hello = (props) => {
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
 const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
+  const name = 'Peter'
+  const age = 10
 
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
   )
 }
